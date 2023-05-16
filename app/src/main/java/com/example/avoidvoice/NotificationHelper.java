f5e97933102592d6221af52d06c64cb97969f063
Kim JaeMin <2098kjm@naver.com>
2023-05-16 17:08:08 +0900

    [VIEW-006] 전화번호 저장 화면 & Notification 구현
    - 전화번호 배열로 저장
    - 전화번호 삭제
    - Notification 뜨고 클릭 시 화면 이동

---
+++ app/src/main/java/com/example/avoidvoice/NotificationHelper.java
@@ -0,0 +1,67 @@
+package com.example.avoidvoice;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.example.avoidvoice.main.MainFragment;
+import com.example.avoidvoice.setting.SetFragment;
+
+public class NotificationHelper extends ContextWrapper {
+
+    public  static final String channelID = "channelID";
+    public  static final String channelName = "channelName";
+
+    private NotificationManager manager;
+
+    public NotificationHelper(Context base){
+        super(base);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            createChannels();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createChannels() {
+        NotificationChannel channel1 = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+        channel1.enableLights(true);
+        channel1.enableVibration(true);
+        channel1.setLightColor(R.color.purple_200);
+        channel1.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        getManager().createNotificationChannel(channel1);
+    }
+
+    public NotificationManager getManager(){
+        if(manager == null){
+            manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        return manager;
+    }
+
+    public NotificationCompat.Builder getChannelNotification(String title, String message){
+        //Notification 클릭 시, 앱 실행
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity
+                (this,0,notificationIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(getApplicationContext(),channelID)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentIntent(notificationPendingIntent)
+                .setAutoCancel(true);
+
+        return notifyBuilder;
+    }
+}
