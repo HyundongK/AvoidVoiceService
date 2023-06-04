61f7659865ff90ff46875c10ecaade22008c3c7d
Sung-geun-Choi <99890543+Sung-geun-Choi@users.noreply.github.com>
2023-06-04 20:19:07 +0900

    [SER-023] 기능 추가 및 보완 - sms 작동 오류 수정

--- app/src/main/java/com/example/avoidvoice/chatapi/APIHandler.java
+++ app/src/main/java/com/example/avoidvoice/chatapi/APIHandler.java
@@ -67,13 +67,12 @@ public class APIHandler {
     public void run(String inputText) throws ExecutionException, InterruptedException {
         ML ml = new ML();
         boolean mlResult = ml.execute(inputText).get();
-        Log.d("ML result", String.valueOf(mlResult));
         if (mlResult) {
             warningCount++;
             Log.d("ML result", String.valueOf(mlResult));
         }
 
-        if (warningCount == 0 && !checkSendMessage) {
+        if (warningCount == 1 && !checkSendMessage) {
             //알림주기
             notificationHelper = new NotificationHelper(context);
             NotificationCompat.Builder nb = notificationHelper.getChannelNotification("알림", "보이스 피싱의 위험이 감지되었습니다.");
